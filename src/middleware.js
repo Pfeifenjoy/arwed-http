ff49266ee2a52d2e93527bd7f406a584ace1366a
Arwed Mett <arwed.mett@googlemail.com>
2016-12-27 01:23:49 +0100

    added root url

--- src/middleware.js
+++ src/middleware.js
@@ -1,40 +1,59 @@
 import Request from "./request"
 
-export default (path, name) => store => next => action => {
-    if(action.payload instanceof Request) {
+function sterilizeRoot(root="") {
+    if(root[root.length] === "/") {
+        return root.slice(0, root.length - 1)
+    }
+    return root
+}
 
-        //check types of parameters
-        if(typeof path !== "string") {
-            throw new TypeError("Path must be a string.")
-        }
-        if(typeof name !== "undefined" && typeof name !== "string") {
-            throw new TypeError("Name must be a string.")
-        }
-
-        //get value out of the store.
-        path = path.split(".")
-        let value = store.getState()
-
-        path.forEach(p => {
-            value = value[p]
-        })
-
-        //determine key
-        let key
-        if(typeof name === "string") {
-            key = name
-        } else {
-            key = path[ path.length - 1 ]
-        }
-
-        //construct payload
-        const payload = {}
-        payload[key] = value
-
-        //fire request
-        action.payload = action.payload.addPayload(payload).send()
-
+export default (path, name, options) => store => {
+    //check types of parameters
+    if(typeof path !== "string") {
+        throw new TypeError("Path must be a string.")
+    }
+    if(typeof name !== "undefined" && typeof name !== "string") {
+        throw new TypeError("Name must be a string.")
     }
 
-    return next(action)
+    //get value out of the store.
+    path = path.split(".")
+    let value = store.getState()
+
+    path.forEach(p => {
+        value = value[p]
+    })
+
+    //determine key
+    let key
+    if(typeof name === "string") {
+        key = name
+    } else {
+        key = path[ path.length - 1 ]
+    }
+
+    return next => action => {
+        if(action.payload instanceof Request) {
+
+            //get url root / prefix
+            let { rootUrl } = options
+            if(rootUrl && action.payload.destination[0] === "/") {
+                rootUrl = sterilizeRoot(rootUrl)
+            } else if (rootUrl && typeof document === "object") {
+                rootUrl = sterilizeRoot(document.pathname)
+            } else {
+                rootUrl = ""
+            }
+            action.payload.destination = rootUrl + action.payload.destination
+
+            //construct payload
+            const payload = {}
+            payload[key] = value
+
+            //fire request
+            action.payload = action.payload.addPayload(payload).send()
+        }
+
+        return next(action)
+    }
 }
